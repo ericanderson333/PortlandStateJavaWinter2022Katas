66095e0ef9ac66f678838a2a1ab25283e4819d3a
ericanderson333 <ericsanderson333@gmail.com>
2022-02-16 19:23:14 -0800

    added BankOCR

---
+++ BankOCR/src/test/java/edu/pdx/cs410J/Eric_London_Santiago_Mina/KataTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.Eric_London_Santiago_Mina;
+
+import org.junit.jupiter.api.Test;
+
+public class KataTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Kata();
+  }
+
+}
